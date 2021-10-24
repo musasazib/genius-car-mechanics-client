be9ecc0cc8bf067d510f38879af82407ed313a9e
musasazib <musasazib@gmail.com>
2021-10-25 03:52:39 +0600

    Add service, Delete servies, Single Service

--- src/Pages/Booking/Booking/Booking.js
+++ src/Pages/Booking/Booking/Booking.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const Booking = () => {
     const { serviceId } = useParams();
+    const [service, setService] = useState({});
+    useEffect(() => {
+        const url = `http://localhost:5000/services/${serviceId}`
+        fetch(url)
+            .then(res => res.json())
+        .then(data=>setService(data))
+    },[serviceId])
     return (
         <div>
+            <h1>Details of: {service.name}</h1>
             <h2>this is booking: {serviceId}</h2>
         </div>
     );
